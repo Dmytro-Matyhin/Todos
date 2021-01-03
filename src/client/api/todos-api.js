9cae0cee40d20673d51474a29b5baa77649f244d
Dmytro <dima.mattock@gmail.com>
2021-01-04 00:46:06 +0200

    add api for todos

---
+++ src/client/api/todos-api.js
@@ -0,0 +1,54 @@
+import {environmentUrls} from '../utils/environment';
+
+const { baseUrl, todosPath } = environmentUrls;
+
+export const getTodosList = () => {
+  return fetch(`${baseUrl}/${todosPath}`)
+  .then(response => response.json())
+  .then(data => data.data)
+  .then(todos => todos)
+}
+
+export const sendTodo = todoData => {
+  return fetch(`${baseUrl}/${todosPath}`, {
+    method: 'POST',
+    body: JSON.stringify(todoData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to POST todos');
+    }
+  })
+}
+
+export const deleteTodo = todoId => {
+  return fetch(`${baseUrl}/${todosPath}/${todoId}`, {
+    method: 'DELETE',
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to DELETE todos');
+    }
+  })
+}
+
+export const updateTodoStatus = (todoId, todoData) => {
+  return fetch(`${baseUrl}/${todosPath}/${todoId}`, {
+    method: 'PUT',
+    body: JSON.stringify({...todoData, todoId}),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to POST todos');
+    }
+  })
+}
