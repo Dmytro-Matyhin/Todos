f62c9cb5949838518d0e7dbafec262bb6c689822
Dmytro <dima.mattock@gmail.com>
2021-01-07 21:06:29 +0200

    add methods for change todo & styles for btns

--- src/client/components/TodoElement/todo-element.jsx
+++ src/client/components/TodoElement/todo-element.jsx
@@ -1,38 +1,93 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 import '../../styles/todo-element.scss'
+
+const Button = ({ text, onClick, disabled = false }) => {
+  const todoItemChange = `${disabled ? 'todo-item-change-save' : 'todo-item-update'}`
+  return (
+    <button   
+      className={todoItemChange}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  )
+}
 
 class TodoElement extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      isEditing: false,
+      newText: props.text
+    }
   }
 
-  shouldComponentUpdate(nextProps) {
-    if (this.props.inputValue === nextProps.inputValue && 
-      this.props.done === nextProps.done) {
-      return false;
-    }
-    return true;
+  handleEdit = _e => {
+    this.setState({ isEditing: true })
+  }
+
+  handleChange = e => {
+    this.setState({ newText: e.target.value })
+  }
+
+  handleUpdate = e => {
+    this.props.changeTodoText(this.props.id, this.state.newText)
+      .then(() => {
+        this.setState({ isEditing: false })
+      })
+  }
+
+  cancelEditing = e => {
+    this.setState({ newText: this.props.text, isEditing: false })
   }
 
   render() {
-    const { text, done, id, changeTodoStatus, delTodo } = this.props;
-    const todoItemDone = `todo-item ${done ? 'todo-item-done' : ''}`;
+    const { text, done, id, changeTodoStatus, deleteTodo } = this.props
+    const todoItemDone = `todo-item ${done ? 'todo-item-done' : ''}`
 
     return (
       <li className={todoItemDone}>
-        <input 
-          type="checkbox"
-          className="todo-item-checkbox"
-          defaultChecked={done}
-          onChange={() => changeTodoStatus(id)}  
-        />
-        <span className="todo-item-text">{text}</span>
-        <button 
-          className="todo-item-button"
-          onClick={() => delTodo(id)}
-        ></button>
+        <div className="todo-item-value">
+          <input
+            type="checkbox"
+            className="todo-item-checkbox"
+            defaultChecked={done}
+            onChange={() => changeTodoStatus(id)}  
+          />
+          {
+            this.state.isEditing
+              ? <input
+                  value={this.state.newText}
+                  onChange={this.handleChange}
+                  className="todo-item-change"
+                />
+              : <span className="todo-item-text">{text}</span>
+          }
+        </div>
+        <div className="todo-item-btns">
+          {
+            this.state.isEditing
+              ? <React.Fragment>
+                  <Button text="Cancel" onClick={this.cancelEditing} />
+                  <Button text="Save" onClick={this.handleUpdate} disabled={this.props.text === this.state.newText} />
+                </React.Fragment>
+              : <Button text="Edit" onClick={this.handleEdit} />
+          }
+
+          <button 
+            className="todo-item-button"
+            onClick={() => deleteTodo(id)}
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </div>
       </li>
     )
   }
@@ -40,7 +95,7 @@ class TodoElement extends React.Component {
 
 TodoElement.propTypes = {
   changeTodoStatus: PropTypes.func,
-  delTodo: PropTypes.func,
+  deleteTodo: PropTypes.func,
   done: PropTypes.bool.isRequired,
   id: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
